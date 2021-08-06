25cd04ecf2288a48b8396304ad8526504553883c
Ananya Rajpurohit <84175666+Ananya296@users.noreply.github.com>
2021-08-06 17:12:29 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,39 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB9teouwXVZJmj9x2qVgaIVHU9akWfC4UI",
+  authDomain: "kwitter-c6c19.firebaseapp.com",
+  databaseURL: "https://kwitter-c6c19-default-rtdb.firebaseio.com",
+  projectId: "kwitter-c6c19",
+  storageBucket: "kwitter-c6c19.appspot.com",
+  messagingSenderId: "952675618901",
+  appId: "1:952675618901:web:cc53806b3b83dacb2b44dc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name +"!";
+    
+    function addRoom()
+    {
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update(
+        {
+          purpose : "adding room name"
+        });
+
+      localStorage.setItem("room_name", room_name);
+      window.location = "kwitter_page.html";
+    }
+
+function logout()
+{
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+
+  window.location = "index.html";
+
+}
